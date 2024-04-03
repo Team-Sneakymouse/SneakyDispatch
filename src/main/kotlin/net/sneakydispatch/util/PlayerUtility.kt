be49395e514e349e744eb590ba150c9067b0c2e1
MomoPewpew <50401575+MomoPewpew@users.noreply.github.com>
2024-04-03 18:03:32 +0200

    Added playerutil

---
+++ src/main/kotlin/net/sneakydispatch/util/PlayerUtility.kt
@@ -0,0 +1,24 @@
+package net.sneakydispatch.util
+
+import org.bukkit.Bukkit
+import org.bukkit.entity.Player
+
+import net.sneakydispatch.SneakyDispatch
+
+object PlayerUtility {
+
+    /**
+     * Returns a list of on-duty paladins.
+     */
+    fun getPaladins(): List<Player> {
+        val paladins: MutableList<Player> = mutableListOf()
+
+        for (player in Bukkit.getOnlinePlayers()) {
+            if (player.hasPermission("$SneakyDispatch.IDENTIFIER.onduty")) {
+                paladins.add(player)
+            }
+        }
+
+        return paladins
+    }
+}
